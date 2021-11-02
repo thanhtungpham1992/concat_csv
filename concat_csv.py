23f56f89336e1677333eef881616bfbcc45f57d8
thanhtungpham1992 <91966837+thanhtungpham1992@users.noreply.github.com>
2021-11-02 13:10:24 +0700

    Add files via upload

--- concat_csv.py
+++ concat_csv.py
@@ -1,44 +1,41 @@
 import streamlit as st
-import os, csv
 import pandas as pd
 import numpy as np
-
+from datetime import datetime
+import io
 
 st.title('Nối File CSV')
 st.write('## Thực hiện nối các file csv cùng định dạng')
+
 with st.form(key='my_form'):
-    path = st.text_input('Nhập đường dẫn tới folder chưa file chỉ chứa CSV')
-    path_op = st.text_input('Nhập đường dẫn lưu file nối (Để trống file sẽ lưu ở Folder gốc)')
-    st.text('Example: C:\Folder\Folder\Folder\...\Folder_csv')
+    lst_files = st.file_uploader("Upload CSV files", accept_multiple_files=True, type=(['csv']))
+    for i in lst_files:
+        if i is not None:
+            print(i.name)
+        else:
+            None
     decode = st.radio('Bảng mã decode', options=['utf8', 'iso-8859-1', 'utf16', 'utf32'])
+    file_type = st.radio('File type export', options=['*.csv','*.xlsx'])
     submit_button = st.form_submit_button(label='Submit')
 
-if submit_button and path != '':
-    files = os.listdir(path)
-    for i in files:
-        if i.lower().find('.csv') == -1:
-            files.remove(i)
-    st.write('## File CSV in folder:')
-    st.text(pd.DataFrame(files, columns=['CSV file']))
-
-    shop = pd.read_csv(path + '\\' + files[0],encoding=decode,index_col=False)   
-    shop['File'] = files[0]
-    for i in range(1,len(files)):
-        temp = pd.read_csv(path + '\\' + files[i],encoding=decode,index_col=False)
-        temp['File'] = files[i]
-        shop = pd.concat([shop,temp])
-    st.write('## Data:')
-    st.dataframe(shop)
-
-    if path_op == '':
-        shop.to_csv(path + '\\' + 'combine_csv.csv', index = False)
-        st.write('## Nối file CSV xong!')
-        st.write('#### Đường dẫn lưu file')
-        st.write(path)
-    else:
-        shop.to_csv(path_op + '\\' + 'combine_csv.csv', index = False)
-        st.write('## Nối file CSV xong!')
-        st.write('#### Đường dẫn lưu file')
-        st.write(path_op)
-else:
-    st.write('## Không tìm thấy link folder chứa file CSV')
+if submit_button:
+    data = pd.read_csv(lst_files[0],encoding=decode,index_col=False)   
+    data['File'] = lst_files[0].name
+    for i in range(1,len(lst_files)):
+        temp = pd.read_csv(lst_files[i],encoding=decode,index_col=False)
+        temp['File'] = lst_files[i].name
+        data = pd.concat([data,temp])
+    st.write('### Data concatenated:')
+    st.dataframe(data)
+    dt_string = datetime.now().strftime("%d%m%Y_%H%M%S")
+    if file_type == '*.csv':
+        st.download_button(data=data.to_csv(index=False, encoding=decode), label='Download CSV File', file_name='concat_file_'+dt_string+'.csv')
+    elif file_type == '*.xlsx':
+        buffer = io.BytesIO()
+        # Create a Pandas Excel writer using XlsxWriter as the engine.
+        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+            # Write each dataframe to a different worksheet.
+            data.to_excel(writer, sheet_name='concat_file', index=False, )
+            # Close the Pandas Excel writer and output the Excel file to the buffer
+            writer.save()
+            st.download_button(label="Download Excel File", data=buffer, file_name='concat_file_'+dt_string+'.xlsx')
