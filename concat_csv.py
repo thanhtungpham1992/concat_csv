add66de1b9af4cfabf4b02a060fc2858c1ecf8ed
thanhtungpham1992 <91966837+thanhtungpham1992@users.noreply.github.com>
2021-11-03 17:00:53 +0700

    Add files via upload

--- concat_csv.py
+++ concat_csv.py
@@ -5,36 +5,42 @@ from datetime import datetime
 import chardet
 import io
 
-st.title('Concatenate')
-st.write('## Thực hiện nối các file csv cùng định dạng')
+st.title('Concatenate file and detect decode')
+st.write('## Nối các file csv cùng định dạng')
 menu = ["Concatenate File", "Check Decode"]
 choice = st.sidebar.selectbox('Menu',menu)
 
 if choice == 'Concatenate File':
     with st.form(key='my_form_1'):
         lst_files = st.file_uploader("Upload CSV files", accept_multiple_files=True, type=(['csv']))
-        decode = st.radio('Bảng mã decode', options=['utf8', 'iso-8859-1', 'ascii','cp1252'])
+        decode = st.radio('Bảng mã decode', options=['utf8', 'iso-8859-1', 'ascii','cp1252', 'Auto detect'])
         file_type = st.radio('File type export', options=['*.csv','*.xlsx'])
         submit_button = st.form_submit_button(label='Submit')
 
     if submit_button:
-        st.write('### Total file:')
+        st.write('### Total file, encoding:')
+        # for i in lst_files:
+        #     if i is not None:
+        #         st.write(i.name)
+        #     else:
+        #         None
+
+        data = pd.DataFrame()
         for i in lst_files:
-            if i is not None:
-                st.write(i.name)
+            if decode == 'Auto detect':
+                encode = chardet.detect(i.read())['encoding']
+                i.seek(0)
             else:
-                None
-        data = pd.read_csv(lst_files[0],encoding=decode,index_col=False)   
-        data['File'] = lst_files[0].name
-        for i in range(1,len(lst_files)):
-            temp = pd.read_csv(lst_files[i],encoding=decode,index_col=False)
-            temp['File'] = lst_files[i].name
+                encode = decode
+            st.write(i.name,encode)
+            temp = pd.read_csv(i,encoding=encode,index_col=False)
+            temp['File'] = i.name
             data = pd.concat([data,temp])
         st.write('### Data concatenated:')
         st.dataframe(data)
         dt_string = datetime.now().strftime("%d%m%Y_%H%M%S")
         if file_type == '*.csv':
-            st.download_button(data=data.to_csv(index=False, encoding=decode), label='Download CSV File', file_name='concat_file_'+dt_string+'.csv')
+            st.download_button(data=data.to_csv(index=False, encoding='UTF-8'), label='Download CSV File', file_name='concat_file_'+dt_string+'.csv')
         elif file_type == '*.xlsx':
             buffer = io.BytesIO()
             # Create a Pandas Excel writer using XlsxWriter as the engine.
@@ -44,9 +50,9 @@ if choice == 'Concatenate File':
                 # Close the Pandas Excel writer and output the Excel file to the buffer
                 writer.save()
                 st.download_button(label="Download Excel File", data=buffer, file_name='concat_file_'+dt_string+'.xlsx')
-elif choice == 'Check Decode':
+elif choice == 'Detect decode':
     with st.form(key='my_form_2'):
-        lst_files = st.file_uploader("Upload CSV files", accept_multiple_files=True, type=(['csv']))
+        lst_files = st.file_uploader('Upload CSV files', accept_multiple_files=True, type=(['csv']))
         # for i in lst_files:
         #     if i is not None:
         #         st.write(i.name)
